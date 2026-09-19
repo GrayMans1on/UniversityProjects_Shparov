744c1bc744c67072932f089628b18ff27c5a6ded
GrayMans1on <v.shparov@mail.ru>
2026-09-19 12:43:43 +0500

    practic_00

---
+++ practics/Practic_00/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Practic_00
+{
+    internal class Program
+    {
+        public static int ConvertToNumber(string str)
+        {
+            int num = 0;
+            foreach (char c in str)
+            {
+                num = num * 10 + (c - '0');
+            }
+            return num;
+        }
+        static void Main()
+        {
+            int num = ConvertToNumber(Console.ReadLine());
+            // ...
+        }
+    }
+}
